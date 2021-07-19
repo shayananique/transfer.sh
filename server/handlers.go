0423a874503f6d01b2f9cf1a6a64e4c08107fa29
Andrea Spacca <andrea.spacca@gmail.com>
2021-07-19 17:21:07 +0200

    reword error

--- server/handlers.go
+++ server/handlers.go
@@ -371,7 +371,7 @@ func (s *Server) postHandler(w http.ResponseWriter, r *http.Request) {
 
 				if status != clamavScanStatusOK {
 					s.logger.Printf("prescan failed: %s",status)
-					http.Error(w, errors.New("Could clamav prescan found a virus").Error(), http.StatusPreconditionFailed)
+					http.Error(w, errors.New("Clamav prescan found a virus").Error(), http.StatusPreconditionFailed)
 					return
 				}
 			}
@@ -540,7 +540,7 @@ func (s *Server) putHandler(w http.ResponseWriter, r *http.Request) {
 
 		if status != clamavScanStatusOK {
 			s.logger.Printf("prescan failed: %s",status)
-			http.Error(w, errors.New("Could clamav prescan found a virus").Error(), http.StatusPreconditionFailed)
+			http.Error(w, errors.New("Clamav prescan found a virus").Error(), http.StatusPreconditionFailed)
 			return
 		}
 
